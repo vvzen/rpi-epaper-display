b2c7413c7f7f03087d55f9dbe2155901873013a9
Valerio Viperino <vv@pc-139-10.customer.greenwich.grosvenor.lan>
2018-02-25 19:18:51 +0000

    fixes

--- drawing_test.py
+++ drawing_test.py
@@ -84,10 +84,13 @@ def main():
             # draw.text((8, 12), 'Hello world!', font=andale_ttf_small, fill = 255)
             draw.rectangle([0, 0, epd2in9.EPD_WIDTH, epd2in9.EPD_HEIGHT], fill=0)
             draw.rectangle([0, 0, 10, 10], fill=255)
+            draw.rectangle([epd2in9.EPD_WIDTH-10, epd2in9.EPD_HEIGHT-10, 10, 10], fill=255)
             draw.text((10, 10), 'pippo', font=andale_ttf_small, fill = 255)
             
             # draw.rectangle([0, 0, epd2in9.EPD_WIDTH, epd2in9.EPD_HEIGHT], fill=255)
             # draw.text((pos_x, pos_y), text, fill=0, font=andale_ttf_small)
+
+            image = image.rotate(270)
 
             # clear memory
             epd.clear_frame_memory(0xFF)
