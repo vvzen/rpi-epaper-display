1e2361e9c622bd2aaca57506b44b11e8e3897fa6
Valerio Viperino <vv@pc-139-10.customer.greenwich.grosvenor.lan>
2018-03-09 00:42:00 +0000

    adding string methods via i2c

--- i2c_example.py
+++ i2c_example.py
@@ -10,18 +10,33 @@ def write_number(value):
     bus.write_byte(ADDRESS, value)
     return -1
 
+def write_string(value):
+    for c in str(value):
+        bus.write_byte(ADDRESS, ord(c)) # send ASCII encoding
+    return -1
+
 def read_number():
     number = bus.read_byte(ADDRESS)
     return number
 
-while True:
-    var = input("Enter 1 - 9: ")
-    if not var:
-        continue
+# def read_string():
+#     number = bus.read_byte(ADDRESS)
+#     return number
 
-    write_number(var)
-    print "Hi, arduino, I sent you: {}".format(var)
-    time.sleep(1)
-
-    number = read_number()
-    print "Arduino: Hey RPI, I received a digit: {}".format(number)
+def main()
+    b = 0
+    while True:
+        # var = input("Enter 1 - 9: ")
+        if b == 0:
+            var = "These aren't the droids you're looking for"
+            # write_number(var)
+            write_string(var)
+            print "Hi, arduino, I sent you: {}".format(var)
+            b += 1
+            time.sleep(1)
+        # number = read_number()
+        # print "Arduino: Hey RPI, I received a digit: {}".format(number)
+try:
+    main()
+except KeyboardInterrupt:
+    pass
