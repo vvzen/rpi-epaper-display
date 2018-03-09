eb953bf953412194283b07f3445a77d07a6df579
Valerio Viperino <vv@pc-139-10.customer.greenwich.grosvenor.lan>
2018-03-09 00:47:16 +0000

    wip..

--- i2c_example.py
+++ i2c_example.py
@@ -12,6 +12,7 @@ def write_number(value):
 
 def write_string(value):
     for c in str(value):
+        print c
         bus.write_byte(ADDRESS, ord(c)) # send ASCII encoding
     return -1
 
@@ -26,16 +27,16 @@ def read_number():
 def main():
     b = 0
     while True:
-        # var = input("Enter 1 - 9: ")
+        var = input("Enter 1 - 9: ")
         if b == 0:
-            var = "These aren't the droids you're looking for"
-            # write_number(var)
-            write_string(var)
+            # var = "These aren't the droids you're looking for"
+            write_number(var)
+            # write_string(var)
             print "Hi, arduino, I sent you: {}".format(var)
             b += 1
             time.sleep(1)
-        # number = read_number()
-        # print "Arduino: Hey RPI, I received a digit: {}".format(number)
+        number = read_number()
+        print "Arduino: Hey RPI, I received a digit: {}".format(number)
 try:
     main()
 except KeyboardInterrupt:
